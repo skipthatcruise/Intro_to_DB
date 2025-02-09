0b6857ef834f0814adf75a09d770e059d13b1c90
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2025-02-09 20:10:53 +0100

    addresses fixed

--- task_6.sql
+++ task_6.sql
@@ -26,9 +26,9 @@ try:
     """
     # Data to be inserted
     customers_data = [
-        (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-        (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-        (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.')
+        (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+        (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+        (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.')
     ]
 
     # Execute the query with multiple rows of data
